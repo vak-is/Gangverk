8f5456191f823bdfacb5323aa2941b6391726f94
B.Z <birkir@vak.is>
2021-09-22 15:58:34 +0000

    Header layout styled

--- components/3-organisms/Header/Header.tsx
+++ components/3-organisms/Header/Header.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link';
+import Image from 'next/image'
+// Styles
+import styles from './Header.module.scss';
+
 const Header = () => {
     return (
-        <div>
+        <header className={styles.header}>
+            <div className={styles.logoContainer}>
+                <Image src="/logo.svg" alt="app logo" width={36} height={36}/>
+            </div>
             <h3>I'm a header, nice to meet you</h3>
-        </div>
+        </header>
     )
 }
 
