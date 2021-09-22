854c7cb91222f6678f9c8addd13f5d3b184461f3
B.Z <birkir@vak.is>
2021-09-22 14:05:54 +0000

    Add Meta component
    Used for dynamic meta tags globally, for the entire aplication.

---
+++ components/0-core/Meta.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+
+type Props = {
+    title: string;
+    description: string;
+}
+
+const Meta = ({ title = 'This is the default title', description = 'this is the default description' }: Props) => {
+    return (
+        <Head>
+             <title>{title}</title>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name="description" content={description}/>
+            <link rel="icon" href="/favicon.svg"/>
+            <link rel="preload" href="/fonts/Poppins/Poppins-Light.ttf" as="font" crossOrigin=""/>
+            <link rel="preload" href="/fonts/Poppins/Poppins-Regular.ttf" as="font" crossOrigin=""/>
+            <link rel="preload" href="/fonts/Poppins/Poppins-Medium.ttf" as="font" crossOrigin=""/>
+            <link rel="preload" href="/fonts/Poppins/Poppins-SemiBold.ttf" as="font" crossOrigin=""/>
+            <link rel="preload" href="/fonts/Poppins/Poppins-Bold.ttf" as="font" crossOrigin=""/>
+            <link rel="preload" href="/fonts/Poppins/Poppins-ExtraBold.ttf" as="font" crossOrigin=""/>
+        </Head>
+    )
+}
+
+export default Meta
