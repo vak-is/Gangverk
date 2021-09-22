19c7f0a421b0b4ecc74233c6457f4ed2f471dc5b
B.Z <birkir@vak.is>
2021-09-22 17:53:18 +0000

    Navigation item to earthquakes added with icon

--- components/3-organisms/Sidebar/Sidebar.tsx
+++ components/3-organisms/Sidebar/Sidebar.tsx
@@ -1,13 +1,24 @@
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image'
 // Styles
 import styles from './Sidebar.module.scss';
+// Components
+import SvgComponent from '../../1-atoms/SVG/earthquake';
 
 const Sidebar = () => {
+    const [isHovering, changeIconColor] = useState(false)
+
     return (
         <aside className={styles.sidebar}>
             <ul>
-                <li><a href="/earthquakes">Earthquakes</a></li>
+                <li 
+                    onMouseEnter={() => changeIconColor(true)}
+                    onMouseLeave={() => changeIconColor(false)}>
+                    <SvgComponent height="24" width="24" stroke={isHovering? '#8BD9BF': '#32292F'}/>
+                    {/* <Image src="/icons/Feather/disc.svg" alt="Earthquake Icon" height={24} width={24}/> */}
+                    <a href="/earthquakes">Earthquakes</a>
+                </li>
             </ul>
         </aside>
     )
