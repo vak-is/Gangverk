b60f003850abf8aa6ac3261305c60754f888e702
B.Z <birkir@vak.is>
2021-09-22 17:52:44 +0000

    Fix font variation imports

--- components/0-core/Meta.tsx
+++ components/0-core/Meta.tsx
@@ -8,17 +8,17 @@ type Props = {
 const Meta = ({ title = 'This is the default title', description = 'this is the default description' }: Props) => {
     return (
         <Head>
-             <title>{title}</title>
+            <title>{title}</title>
             <meta charSet="utf-8" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <meta name="description" content={description}/>
             <link rel="icon" href="/logo.svg"/>
-            <link rel="preload" href="/fonts/Poppins/Poppins-Light.ttf" as="font" crossOrigin=""/>
-            <link rel="preload" href="/fonts/Poppins/Poppins-Regular.ttf" as="font" crossOrigin=""/>
-            <link rel="preload" href="/fonts/Poppins/Poppins-Medium.ttf" as="font" crossOrigin=""/>
-            <link rel="preload" href="/fonts/Poppins/Poppins-SemiBold.ttf" as="font" crossOrigin=""/>
-            <link rel="preload" href="/fonts/Poppins/Poppins-Bold.ttf" as="font" crossOrigin=""/>
-            <link rel="preload" href="/fonts/Poppins/Poppins-ExtraBold.ttf" as="font" crossOrigin=""/>
+            <link rel="preload" href="/fonts/Titillium-Web/TitilliumWeb-Light.ttf" as="font" crossOrigin=""/>
+            <link rel="preload" href="/fonts/Titillium-Web/TitilliumWeb-Regular.ttf" as="font" crossOrigin=""/>
+            <link rel="preload" href="/fonts/Titillium-Web/TitilliumWeb-ExtraLight.ttf" as="font" crossOrigin=""/>
+            <link rel="preload" href="/fonts/Titillium-Web/TitilliumWeb-SemiBold.ttf" as="font" crossOrigin=""/>
+            <link rel="preload" href="/fonts/Titillium-Web/TitilliumWeb-Bold.ttf" as="font" crossOrigin=""/>
+            <link rel="preload" href="/fonts/Titillium-Web/TitilliumWeb-Black.ttf" as="font" crossOrigin=""/>
         </Head>
     )
 }
