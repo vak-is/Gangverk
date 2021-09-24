c1de965951ae2bcae649567761e6339bb2356434
B.Z <birkir@vak.is>
2021-09-24 18:21:43 +0000

    Map placholder if need for future use

---
+++ components/3-organisms/Map/Map.tsx
@@ -0,0 +1,11 @@
+import styles from './Map.module.scss';
+
+const Map = () => {
+    return (
+        <section className={`box placeholder ${styles.map}`}>
+            <h1>For map</h1>
+        </section>
+    )
+}
+
+export default Map
