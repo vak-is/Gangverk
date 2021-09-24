cc4e3b9d137a82faf0ea773d7ab99d9de47b0f1c
B.Z <birkir@vak.is>
2021-09-24 18:17:35 +0000

    Sorting functions and additional styling

--- components/3-organisms/Table/Table.tsx
+++ components/3-organisms/Table/Table.tsx
@@ -1,5 +1,9 @@
 
+import React from 'react';
 import styles from './Table.module.scss';
+// Components 
+import ChevronIcon from '../../1-atoms/SVG/chevron';
+import Earthquakes from '../../../pages/earthquakes';
 
 // TODO: Find better name
 type EarthquakeType = {
@@ -16,18 +20,92 @@ type EarthquakeType = {
 type EarthquakeDataType = Array<EarthquakeType>
 
 type TableProps = {
-    earthquakes: EarthquakeDataType
+    earthquakes: EarthquakeDataType,
+}
+
+type sortOptionsType = {
+    key: keyof EarthquakeType,
+    direction: string
+}
+
+const formatDate = (timestamp: string) => {
+    const date = new Date(timestamp);
+    return date.toLocaleDateString();
+}
+
+const sortItems = (earthquakes: EarthquakeDataType, options: null | sortOptionsType = null) => {
+    const [sortOptions, setSortingOptions] = React.useState(options);
+
+    const sortedEarthquakes = React.useMemo(() => {
+        let sortableItems = [...earthquakes];
+        
+        if (sortOptions !== null) {
+            sortableItems.sort((a , b) => {
+                if (a[sortOptions.key] < b[sortOptions.key]) {
+                    return sortOptions.direction === 'ascending' ? -1 : 1;
+                }
+                if (a[sortOptions.key] > b[sortOptions.key]) {
+                    return sortOptions.direction === 'ascending' ? 1 : -1;
+                }
+                return 0;
+            });
+        }
+        return sortableItems;
+    }, [earthquakes, sortOptions])
+
+    const requestSort = (key: keyof EarthquakeType) => {
+        let direction = 'ascending';
+        if(
+            sortOptions && 
+            sortOptions.key === key &&
+            sortOptions.direction === 'ascending'
+        ) {
+            direction = 'descending'
+        }
+        setSortingOptions({key, direction})
+    };
+
+    return {earthquakes: sortedEarthquakes, requestSort, sortOptions };
+
 }
 
 const Table = (props: TableProps) => {
-    console.log(props.earthquakes)
+    const {earthquakes, requestSort, sortOptions } = sortItems(props.earthquakes)
+    const getClassFor = (name: string) => {
+        if (!sortOptions) {
+            return;
+        }
+        
+        return sortOptions.key === name ? sortOptions.direction : 'default'
+    }
+
     return (
         <>
-            {props.earthquakes && 
+            {earthquakes && 
                 <table className={styles.table}>
+                    <thead> 
+                        <tr>
+                            <th>
+                                <div className={`${styles.magnitudeHeader} ${getClassFor('size')}`} onClick={() => requestSort('size')}>
+                                    <span>
+                                        Size
+                                    </span>  
+                                    <ChevronIcon height="16px" width="16px"/>
+                                </div>
+                            </th>
+                            <th>
+                                <div className={`${styles.descriptionHeader} ${getClassFor('timestamp')}`} onClick={() => requestSort('timestamp')}>
+                                    <span>
+                                        Date
+                                    </span>  
+                                    <ChevronIcon height="16px" width="16px"/>
+                                </div>
+                            </th>
+                        </tr>
+                    </thead>
                     <tbody>
-                        {props.earthquakes.map((earthquake, index) => (
-                            <tr>
+                        {earthquakes.map((earthquake, index) => (
+                            <tr key={earthquake.timestamp}>
                                 <td>
                                     <div className={styles.magnitude}>
                                         <span>
@@ -38,19 +116,12 @@ const Table = (props: TableProps) => {
                                 <td>
                                     <div className={styles.description}>
                                         <header>{earthquake.humanReadableLocation}</header>
-                                        <span className={styles.timestamp}>{earthquake.timestamp}</span>
+                                        <span className={styles.timestamp}>{formatDate(earthquake.timestamp)}</span>
                                     </div>
                                 </td>
                             </tr>
                         ))}
                     </tbody>
-                    {/* <tfoot>
-                        <tr>
-                            <td>Centro comercial Moctezuma</td>
-                            <td>Francisco Chang</td>
-                            <td>Mexico</td>
-                        </tr>
-                    </tfoot> */}
                 </table>
             }
         </>
