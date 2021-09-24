5795c297196a757b57a9087c1e00d543a4991e81
B.Z <birkir@vak.is>
2021-09-24 18:20:22 +0000

    Placeholders added on home page for analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,20 @@ import type { NextPage } from 'next'
 import Layout from '../components/0-core/Layout/Layout'
 
 const Home: NextPage = () => {
-  return (
-    <Layout title="This is a title" description="This is a description">
-      <h1>
-        Gangverk Project
-      </h1>
-    </Layout>
-  )
+    return (
+        <Layout title="Home | Gangverk Earthquake Project" description="Welcome to the beginning of a great app">
+            <h1>
+                Gangverk Project
+            </h1>
+
+            <div className="container even fourtimesfour">
+                <section className="box analytics placeholder">Analytics placeholder</section>
+                <section className="box analytics placeholder">Analytics placeholder</section>
+                <section className="box analytics placeholder">Analytics placeholder</section>
+                <section className="box analytics placeholder">Analytics placeholder</section>
+            </div>
+        </Layout>
+    )
 }
 
 export default Home
